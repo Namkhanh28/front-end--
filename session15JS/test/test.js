bf47f40a1454aed5ffa084d17303fb366006608e
Nam Khanh <nguyendanhnamkhanh06@gmail.com>
2026-03-06 13:44:50 +0700

    rest

---
+++ session15JS/test/test.js
@@ -0,0 +1,11 @@
+let input = document.getElementById("email");
+let pass = document.getElementById("password");
+let confirm = document.getElementById("confirm-password");
+let form = document.getElementsByClassName("register-form");
+
+form.addEventListener("submit", (form) => {
+  form.input.value;
+  form.pass.value;
+  form.confirm.value;
+  form.preventDefault();
+});
